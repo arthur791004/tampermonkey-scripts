90c9e26ee429b99cdc1c5d5fafc899d3deed86ad
arthur791004 <arthur791004@gmail.com>
2023-02-01 17:46:30 +0800

    Use timer to check the main player exists or not

--- youtube-helper.user.js
+++ youtube-helper.user.js
@@ -10,7 +10,7 @@
 
 (function() {
     'use strict';
-    
+
     const ELEMENTS = {
         MAIN_PLAYER: 'ytd-player video.html5-main-video',
         CONFIRM_DIALOG_CONTINUE_BUTTON: 'yt-confirm-dialog-renderer .yt-spec-button-shape-next--call-to-action',
@@ -29,17 +29,24 @@
           }
         `);
     }
-    
+
     function enableAutoContinueIfThePlayerPausedByConfirmDialog() {
-        const mainPlayer = document.querySelector(ELEMENTS.MAIN_PLAYER);
-        if (mainPlayer) {
+        /**
+         * Use timer to check whether the main player exists or not.
+         */
+        window.setTimeout(() => {
+            const mainPlayer = document.querySelector(ELEMENTS.MAIN_PLAYER);
+            if (!mainPlayer) {
+                enableAutoContinueIfThePlayerPausedByConfirmDialog();
+                return;
+            }
             mainPlayer.addEventListener('pause', () => {
                 const continueButton = document.querySelector(ELEMENTS.CONFIRM_DIALOG_CONTINUE_BUTTON);
                 if (continueButton) {
                     continueButton.click();
                 }
             });
-        }     
+        }, 30 * 1000);
     }
 
     function addGlobalStyle(css) {
